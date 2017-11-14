0c499bc9d5b86683690bfdc3bcdcaeb51141ae1a
971976839@qq.com <lhygit2016>
2017-11-14 15:48:58 +0800

    登录页

--- src/controller/base.js
+++ src/controller/base.js
@@ -1,5 +1,14 @@
 module.exports = class extends think.Controller {
-  __before() {
-
+  async __before() {
+    if(this.ctx.controller === 'admin' && this.ctx.action === 'index' || this.ctx.action ==='login'){
+      //此时正在登陆页面 不验证 直接返回
+        return;
+    }
+    let userinfo=await this.session('userinfo');
+    if(!think.isEmpty(userinfo)){
+      this.assign('userinfo',userinfo);
+    }else {
+      return this.redirect('/admin/index');
+    }
   }
 };
