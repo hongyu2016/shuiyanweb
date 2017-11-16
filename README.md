355ea55a176cdd40e2083d0e9eb3a9d0eda33750
971976839@qq.com <lhygit2016>
2017-11-16 17:43:30 +0800

    左侧菜单

--- README.md
+++ README.md
@@ -21,3 +21,4 @@ Use pm2 to deploy app on production enviroment.
 ```
 pm2 startOrReload pm2.json
 ```
+##调试模式下运行：先set DEBUG=think-router然后 node development.js
--- src/controller/base.js
+++ src/controller/base.js
@@ -7,6 +7,25 @@ module.exports = class extends think.Controller {
         }
     }else {
         this.assign('userinfo',userinfo);
+        let menuList=await this.model('menu').order('menu_id ASC, menu_name DESC').select(); //菜单
+        //this.assign('menuList',menuList);
+
+        let oneMenu=[];
+        let subMenu=[];
+        for (let i in menuList){
+            if(menuList[i].parent_menu==-1 ){
+                oneMenu.push(menuList[i]);
+            }
+            else{
+                subMenu.push(menuList[i]);
+            }
+        }
+        this.assign({
+            'oneMenu':oneMenu,
+            'subMenu':subMenu
+        });
+
     }
   }
+
 };
