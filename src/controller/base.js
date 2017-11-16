e6fd346e884a6d56e453d54ce851081ebdf97d3a
971976839@qq.com <lhygit2016>
2017-11-16 18:43:08 +0800

    左侧菜单

--- src/controller/base.js
+++ src/controller/base.js
@@ -25,6 +25,44 @@ module.exports = class extends think.Controller {
             'subMenu':subMenu
         });
 
+        //重新组成新数组
+        function format(list) {
+            let arr = [];
+            list.map(function(item, index) {
+                if (item.parent_menu == -1) {
+                    let o = arr.find(x => {
+                        return x.menu_id == item.menu_id
+                    })
+                    if (!o) {
+                        o = {
+                            child_menu: []
+                        }
+                        arr.push(o)
+                    }
+                    Object.assign(o, item)
+
+                }else{
+                    let o=arr.find(x=>{
+                        return x.menu_id==item.parent_menu;
+                    })
+                    if(!o){
+                        o={
+                            child_menu:[item],
+                            menu_id:item.parent_menu
+                        }
+                        arr.push(o)
+                    }else{
+                        o.child_menu.push(item)
+                    }
+                }
+
+            })
+            return arr;
+        }
+
+       let newMenu= format(menuList);
+        this.assign('newMenu',newMenu);
+
     }
   }
 
