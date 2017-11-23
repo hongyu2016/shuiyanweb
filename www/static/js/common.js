e3f13211083e66b512bc4c96402cda008e4710fc
971976839@qq.com <lhygit2016>
2017-11-23 13:37:46 +0800

    菜单显示

--- www/static/js/common.js
+++ www/static/js/common.js
@@ -7,3 +7,15 @@ function GetQueryString(name)
     var r = window.location.search.substr(1).match(reg);
     if(r!=null)return  unescape(r[2]); return null;
 }
+$(function () {
+    var meneuId=GetQueryString('menu_id');
+    var $sidebarMenu=$('.sidebar-menu');
+    $sidebarMenu.find('li').each(function (index,ele) {
+        var dataId=$(this).attr('data-id');
+        if(dataId==meneuId){
+            $(this).addClass('active');
+            $(this).closest('.treeview').addClass('menu-open');
+            $(this).closest('.treeview-menu').slideDown();
+        }
+    })
+});
