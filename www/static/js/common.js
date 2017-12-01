67aa79bfbf46924c53c24c380c5c9fb1cbdf9786
971976839@qq.com <lhygit2016>
2017-12-01 17:56:19 +0800

    正计时

--- www/static/js/common.js
+++ www/static/js/common.js
@@ -7,6 +7,23 @@ function GetQueryString(name)
     var r = window.location.search.substr(1).match(reg);
     if(r!=null)return  unescape(r[2]); return null;
 }
+/*
+* 时间正计时
+* */
+function showTime() {
+    var nowTime=new Date(); //new当前时间
+    var year=nowTime.getFullYear();//完整年份
+    var month=nowTime.getMonth()+1; //月份
+    var day=nowTime.getDate();  //日
+    var hour=nowTime.getHours();//时
+    var min=nowTime.getMinutes()+1;//分钟
+    var second=nowTime.getSeconds()+1;//秒
+    hour=hour<9?'0'+hour:hour;
+    min=min<9?'0'+min:min;
+    second=second<9?'0'+second:second;
+    var fullTime=year+'年'+month+'月'+day+'日 '+hour+':'+min+':'+second
+    $('#show-time').html(fullTime);
+}
 $(function () {
     var meneuId=GetQueryString('menu_id');
     var $sidebarMenu=$('.sidebar-menu');
@@ -17,5 +34,8 @@ $(function () {
             $(this).closest('.treeview').addClass('menu-open');
             $(this).closest('.treeview-menu').slideDown();
         }
-    })
+    });
+    //时间正计时
+    setInterval('showTime()',50)
+
 });
