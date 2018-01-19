60a48488b8d02ea3b53d28b869c8c25d41cdf6fc
971976839@qq.com <lhygit2016>
2018-01-19 11:40:42 +0800

    公告通知使用socket

--- src/api/controller/websocket.js
+++ src/api/controller/websocket.js
@@ -4,20 +4,23 @@ module.exports = class extends Base {
 		super(...arg);
 	}
 	async openAction() {
-		let data=await this.model('notice').limit(3).select();
+		let data=await this.model('notice').limit().select();
 		this.emit('noticeList', data);//给当前 socket 发送事件
-		//this.broadcast('joined', '新的连接加入 successfully!');//给所有的 socket 广播事件
+		this.broadcast('joined', '新的连接成功加入!');//给所有的 socket 广播事件
 	}
 	closeAction() {
-		console.log('ws 关闭');
-
+		console.log('socket 关闭');
 	}
-	getNoticeAction(){
+	async getNoticeAction(){
 		/*console.log('获取客户端 addUser 事件发送的数据', this.wsData);
 		console.log('获取当前 WebSocket 对象', this.websocket);
 		console.log('判断当前请求是否是 WebSocket 请求', this.isWebsocket);*/
 
-		//this.emit('noticeList','This client ss试试 successfully!')
+		let thisVal=this.wsData;
+		if(thisVal=='更改通知数据'){
+			let data=await this.model('notice').limit().select();
+			this.broadcast('noticeList', data);
+		}
 
 	}
 };
